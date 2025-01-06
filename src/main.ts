a49f5b3d4ff73da1582e421b2085e1aaf7d13d4d
SandadiNikhil <101047383+SandadiNikhil@users.noreply.github.com>
2025-01-06 03:55:53 -0800

    Delete src directory

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withJsonpSupport } from '@angular/common/http';
-import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, {
-    providers: [
-      provideHttpClient(withJsonpSupport()) 
-    ],
-  }).catch((err) => console.error(err));
