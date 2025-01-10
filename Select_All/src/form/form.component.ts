2ebf5b65567c929d7075d4ca0fb9c891c7b4688a
SandadiNikhil <101047383+SandadiNikhil@users.noreply.github.com>
2025-01-10 10:23:45 -0800

    commit select

---
+++ Select_All/src/form/form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [CommonModule, FormsModule], 
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+
+export class FormComponent implements OnInit {
+  movies: string[] = [
+    'Changjinhu (2021)',
+    'Dune (2021)',
+    'Shang-Chi and the Legend of the Ten Rings (2021)',
+    'Free Guy (2021)',
+    'The Many Saints of Newark (2021)',
+    'Finch (2021)',
+    'Candyman (2021)',
+    'No Time to Die (2021)',
+    'Halloween Kills (2021)',
+  ];
+
+  selectall = false;
+  obj: { [key: string]: boolean } = {};
+
+  constructor() {}
+
+  ngOnInit() {
+    // Initialize all movies as unchecked
+    for (let movie of this.movies) {
+      this.obj[movie] = false;
+    }
+  }
+
+  selectallHandler() {
+    for (let key in this.obj) {
+      this.obj[key] = this.selectall;
+    }
+  }
+
+  changeHandler(movie: string) {
+    const checkedCount = Object.values(this.obj).filter(Boolean).length;
+    this.selectall = (checkedCount === this.movies.length);
+  }
+}
